4a5254165021a6dce626f86e15b6cf98a7f5ec17
Anas <anasshamoon12002@yahoo.com>
2023-01-08 19:16:54 +0100

    feat(winner-logic): Add function to check the winner in rows

--- TTT.py
+++ TTT.py
@@ -25,3 +25,24 @@ class TTT:
     def get_player_mark(self):
         pass
 
+    # Checks if there is a winner based upon the rules of tic tac toe
+    def check_winner(self):
+        pass
+
+    def check_rows(self):
+        for i in range(len(self.board)):
+            if self.check_row_and_col(self.board[0][i], self.board[1][i], self.board[2][i]):
+                return True
+        return False
+
+    def check_columns(self):
+        pass
+
+    def check_diagonals(self):
+        pass
+
+    # Checks if the values are same and are not empty. Used in checking the rows, columns and diagonals for the winner
+    def check_row_and_col(self, val_1, val_2, val_3):
+        return val_1 != "   " and val_1 == val_2 and val_2 == val_3
+
+
