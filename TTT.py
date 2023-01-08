fbfa60c3d63a30be34a85102e2e735de3548de28
noor71929 <noor-71929@hotmail.com>
2023-01-08 18:43:14 +0100

    initialized the TTT game

--- TTT.py
+++ TTT.py
@@ -1,3 +1,19 @@
 class TTT:
     def __init__(self):
-        pass
+        self.board = [[""for j in range (3)] for i in range(3)]
+        self.player_mark = ""
+
+    def initialize_bord(self):
+       for i in range(len(self.board)):
+          for j in range(len(self.board[i])):
+
+                 self.board[i][j] = "   "
+
+    def display_board(self):
+        print("----------------------")
+        for i in range(3):
+            print("|", end="")
+            for j in range(3):
+                print(self.board[i][j] + " | ", end="")
+            print()
+            print("--------------------")
