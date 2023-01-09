bb51f4c8becf44037c0c914e1229e619775280a0
Anas <anasshamoon12002@yahoo.com>
2023-01-09 19:42:14 +0100

    feat(computer-rand): Create logic to enable the computer make the moves using random nnumbers

--- Players.py
+++ Players.py
@@ -1,4 +1,5 @@
 from TTT import TTT
+import random
 
 class Players:
     def __init__(self):
@@ -32,8 +33,54 @@ class Players:
 
 
 
-    def random_computer(self):
-        pass
+    def random_computer(self, turn):
+        count_computer = turn
+
+        game = TTT()
+        game.set_player(count_computer)
+        game.display_board()
+
+        while True:
+
+            print("\nCurrent Game")
+
+            if count_computer % 2 == 0:
+                print("\nYou: X")
+                self.row = int(input("Enter number of row: "))
+                self.column = int(input("Enter number of column: "))
+                if self.row > 0 and self.row < 4 and self.column > 0 and self.column < 4 and game.value_check(self.row,
+                                                                                                              self.column):
+                    game.modify_board(self.row, self.column)
+
+                else:
+                    print("\nWrong Input!!! Try again!!! \n")
+
+            else:
+                print("\nComputer: 0")
+                while True:
+                    self.row = random.randint(1, 3)
+                    self.column = random.randint(1, 3)
+
+                    if game.value_check(self.row, self.column):
+                        game.modify_board(self.row, self.column)
+                        break
+
+            if game.check_winner():
+                print()
+                game.display_board()
+
+                if count_computer % 2 == 0:
+                    print("\nYou (X) are the winner!!! \n")
+                else:
+                    print("\nComputer (0) is the winner!!! \n")
+
+                break
+
+            count_computer += 1
+
+        if game.board_check() and game.check_winner():
+            print("\nThis game is a tie!\n")
+
 
     def AI_computer(self):
         pass
