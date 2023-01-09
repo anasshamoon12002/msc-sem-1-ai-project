bd76f88275974cf2a109341264dde44da6cf7586
noor71929 <noor-71929@hotmail.com>
2023-01-09 17:19:53 +0100

    created signatures of the main functions of the class

--- Players.py
+++ Players.py
@@ -1,3 +1,13 @@
 class Players:
     def __init__(self):
+        self.row = 0
+        self.column = 0
+
+    def multiplayer(self):
+        pass
+
+    def random_computer(self):
+        pass
+
+    def AI_computer(self):
         pass
